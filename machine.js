20e75b2cb5837dca531a2233695968e6e8bc9e37
Sam Glider <smahaseeb@hotmail.com>
2013-08-03 07:53:20 +0530

    Update machine.js

--- machine.js
+++ machine.js
@@ -1,1 +1,13 @@
-// Coming soon...
+// The list of Ids.
+var ssnIds = [];
+// Create a new Id.
+var createId = function(ssnId, name, age, gender, phone, occupation, threatLevel, knowMachine) {
+    this.ssnId = ssnId;
+    this.name = name;
+    this.age = age;
+    this.gender = gender;
+    this.phone = occupation;
+    this.threatLevel = threatLevel;
+    this.knowMachine = knowMachine;
+}
+// 
